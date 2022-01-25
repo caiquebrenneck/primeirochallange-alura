ec314027a31162faa6326487ee6329042fc82084
Caique Aparecido Rodrigues de Sousa <caique.rodrigues99@hotmail.com>
2022-01-25 01:08:36 -0300

    mensageminvalidacodificar
    Adicionei estilo se o usuário inserir uma mensagem inválida para CODIFICAR.

--- js/botaolimpar.js
+++ js/botaolimpar.js
@@ -1,5 +1,9 @@
 var botaoLimpar = document.querySelector("#limpar");
 botaoLimpar.onclick = function () {
+    document.querySelector("#mensagem-do-usuario").classList.remove("invalido");
+    document.querySelector("#mensagem-do-usuario").classList.add("valido");
+    document.querySelector("#mensagem-traduzido").classList.remove("invalido");
+    document.querySelector("#mensagem-traduzido").classList.add("valido");
     document.querySelector("#mensagem-traduzido").value = "";
     document.querySelector("#mensagem-do-usuario").value = "";
     var fundoPensando = document.querySelector("#mensagem-traduzido");
